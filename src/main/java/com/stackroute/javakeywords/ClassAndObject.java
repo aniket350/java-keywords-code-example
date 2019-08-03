c3d8660af53f74cd69839fd3babe9c610b0eec78
aniket350 <subarni100@gmail.com>
2019-08-03 09:54:16 +0530

    added readme and gitignore file and completed classandobject task

---
+++ src/main/java/com/stackroute/javakeywords/ClassAndObject.java
@@ -0,0 +1,42 @@
+//More on Classes and Objects - Anatomy of a class, Constructors, Creating and using objects, this keyword, Controlling access to a member
+//OOPs Concepts - Object, Class,Package
+
+package com.stackroute.javakeywords;
+
+public class ClassAndObject {
+
+    private String Name;
+    private  int age;
+
+    public ClassAndObject() {
+    }
+
+    public ClassAndObject(String name, int age) {
+        Name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "ClassAndObject{" +
+                "Name='" + Name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ src/main/java/com/stackroute/javakeywords/MainClass.java
@@ -0,0 +1,11 @@
+package com.stackroute.javakeywords;
+
+public class MainClass {
+    public static void main(String[] args){
+        ClassAndObject classAndObject = new ClassAndObject();
+        classAndObject.setName("Aniket");
+        classAndObject.setAge(46);
+        System.out.println(classAndObject.getName());
+        System.out.println(classAndObject.getAge());
+    }
+}
